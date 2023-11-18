df9e9ed26b43f6dfdbd2306a253b79f6dff20336
kirill-seliverstov <kirill.seliverstov1701@gmail.com>
2023-11-18 17:12:28 +0300

    gtm-module

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from './store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: "GTM-WDQWBFD7",
+};
+
+TagManager.initialize(tagManagerArgs);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
@@ -14,4 +21,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
